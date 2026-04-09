45e299c9094b15f759d80cd24adc53e133eba3dc
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2026-04-08 21:14:07 -0500

    Added type for formData and useState. Also added the handleChange and handleSubmit functions.

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,6 +1,42 @@
+import { useState } from "react";
+
+type FormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  queryType: string;
+  message: string;
+  consent: boolean;
+};
+
 function ContactForm() {
+  const [formData, setFormData] = useState<FormData>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    message: "",
+    queryType: "",
+    consent: false,
+  });
+
+  const handleChange: React.ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = (e) => {
+    const { name, value, type } = e.target;
+    const checked =
+      type === "checkbox" ? (e.target as HTMLInputElement).checked : false;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
+
+  const handleSubmit: React.SubmitEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h1>Contact Us</h1>
 
       <div className="form__row">
@@ -8,14 +44,26 @@ function ContactForm() {
           <label htmlFor="firstName">
             First Name <span aria-hidden="true">*</span>
           </label>
-          <input type="text" id="firstName" name="firstName" required />
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            required
+            onChange={handleChange}
+          />
         </div>
 
         <div className="form__group">
           <label htmlFor="lastName">
             Last Name <span aria-hidden="true">*</span>
           </label>
-          <input type="text" id="lastName" name="lastName" required />
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            required
+            onChange={handleChange}
+          />
         </div>
       </div>
 
@@ -23,7 +71,13 @@ function ContactForm() {
         <label htmlFor="email">
           Email Address <span aria-hidden="true">*</span>
         </label>
-        <input type="email" id="email" name="email" required />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          required
+          onChange={handleChange}
+        />
       </div>
 
       <fieldset>
@@ -32,11 +86,22 @@ function ContactForm() {
         </legend>
         <div className="form__radio-group">
           <label>
-            <input type="radio" name="queryType" value="general" required />
+            <input
+              type="radio"
+              name="queryType"
+              value="general"
+              required
+              onChange={handleChange}
+            />
             General Enquiry
           </label>
           <label>
-            <input type="radio" name="queryType" value="support" />
+            <input
+              type="radio"
+              name="queryType"
+              value="support"
+              onChange={handleChange}
+            />
             Support Request
           </label>
         </div>
@@ -46,11 +111,22 @@ function ContactForm() {
         <label htmlFor="message">
           Message <span aria-hidden="true">*</span>
         </label>
-        <textarea id="message" name="message" required></textarea>
+        <textarea
+          id="message"
+          name="message"
+          required
+          onChange={handleChange}
+        ></textarea>
       </div>
 
       <div className="form__group">
-        <input type="checkbox" id="consent" name="consent" required />
+        <input
+          type="checkbox"
+          id="consent"
+          name="consent"
+          required
+          onChange={handleChange}
+        />
         <label htmlFor="consent">
           I consent to being contacted by the team{" "}
           <span aria-hidden="true">*</span>
